a7f55411ae54f76c479cc7adcae4afc3bbe3af00
mwdchang <mwdchang@gmail.com>
2014-09-05 09:47:27 -0400

    update

--- runTest.js
+++ runTest.js
@@ -8,7 +8,7 @@ var params = require("./params");
 
 // var url = "https://dcc.icgc.org/api/v1";
 var url = "https://localhost:55555/api/v1";
-var eventCount = 50;
+var eventCount = 3;
 
 function _project_gene_count(proj) {
    if (! Array.isArray(proj)) proj = [proj];
@@ -49,6 +49,26 @@ function _project_mutation_count(proj) {
    });
 }
 
+function _project_donor_count(proj) {
+   if (! Array.isArray(proj)) proj = [proj];
+
+   request(url + "/projects/" + proj.join(',') +  "/donors/counts", function(err, response, body) {
+      var data = JSON.parse(body);
+      var filter = {donor:{projectId:{is: proj }}};
+      var u = "/donors?filters=" + JSON.stringify(filter) + "&size=0";
+      request(url + u, function(err, response, body) {
+         body = JSON.parse(body);
+         a2 = body.pagination.total;
+         a1 = data.Total;
+
+         console.log("Project-Donor", proj, "Expecting:", a1, "Actual:", a2);
+         assert.equal(a1, a2);
+      });
+   });
+}
+
+
+
 // v1/projects/{projectIds}/genes/{geneIds}/mutations/count
 function _project_gene_mutation_count(proj, gene) {
    if (! Array.isArray(proj)) proj = [proj];
@@ -80,6 +100,34 @@ function _project_gene_mutation_count(proj, gene) {
 }
 
 
+function _project_gene_donor_count(proj, gene) {
+   if (! Array.isArray(proj)) proj = [proj];
+   if (! Array.isArray(gene)) gene = [gene];
+
+   var path = proj.join(',') + "/genes/" + gene.join(',') + "/donors/counts";
+   request(url + "/projects/" + path , function(err, response, body) {
+      console.log("path ", "/projects/"+path);
+      var data = JSON.parse(body);
+
+      proj.forEach(function(p) {
+         var filter = {
+            donor:{projectId:{is: p}},
+            gene:{id:{is: gene}}
+         };
+         var u = "/donors?filters=" + JSON.stringify(filter) + "&size=0";
+         request(url + u, function(err, response, body) {
+            body = JSON.parse(body);
+            a2 = body.pagination.total;
+            a1 = data[p].Total;
+            console.log("Project-Gene-Donor", p, gene.join(','), "Expecting:", a1, "Actual:", a2);
+            assert.equal(a1, a2);
+         });
+      });
+   });
+}
+
+
+
 function test_project_gene_mutation_count_specific() {
    _project_gene_mutation_count("BRCA-US", ["ENSG00000204969", "ENSG00000204970"]);
 }
@@ -109,6 +157,19 @@ function test_project_gene_mutation_count_random() {
 }
 
 
+function test_project_gene_donor_count_random() {
+   var a1;
+   var a2;
+   for (var i=0; i < eventCount; i++) {
+      var sampleSize1 = Math.floor(Math.random() * params.projects.length) + 1;
+      var proj = underscore.sample( params.projects, sampleSize1);
+      var sampleSize2 = Math.floor(Math.random() * params.genes.length) + 1;
+      var gene = underscore.sample( params.genes, sampleSize2);
+      _project_gene_donor_count(proj, gene);
+   }
+}
+
+
 function test_project_gene_count_random() {
    var a1;
    var a2;
@@ -131,10 +192,14 @@ function test_project_mutation_count_random() {
 }
 
 
+/*
 test_project_mutation_count_specific();
 test_project_gene_count_specific();
 test_project_gene_mutation_count_specific();
+*/
 
 test_project_mutation_count_random();
 test_project_gene_count_random();
 test_project_gene_mutation_count_random();
+test_project_gene_donor_count_random();
+_project_donor_count( ['BRCA-US', 'ALL-US'] );
